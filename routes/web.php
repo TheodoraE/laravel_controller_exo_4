3c152f728d7a4704057bf4686f3f55f8fe9ca5ca
TheodoraE <tidoraa@gmail.com>
2021-01-29 17:03:55 +0100

    structure

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// laravel_controller_exo_4
+// Integration d'un tempalte multi-page, toutes les données du sites doivent faire partie de variable objet qui sont renvoyées dans des views
+// Un controller par view
+// Utilisation des templates ( template,yield, etc.. )
+// Utilisation de git
+
 Route::get('/', function () {
     return view('welcome');
 });
